420cab697b406530f155937db79363ad4dfb8105
miriamschaefer <msuarezmelian@gmail.com>
2020-07-12 13:44:48 +0100

    hide sections

--- src/js/01-hidden-sections.js
+++ src/js/01-hidden-sections.js
@@ -1,7 +1,7 @@
 const click = document.querySelector('.click');
-const section = document.querySelector('.colors-container');
-const arrowDown = document.querySelector('.moonicondown');
-const arrowUp = document.querySelector('.mooniconup');
+const section = document.querySelector('.js-hide-favs');
+const arrowDown = document.querySelector('.js-arrow-down');
+const arrowUp = document.querySelector('.js-arrow-up');
 
 function displayArrow(ev) {
   ev.preventDefault();
@@ -11,32 +11,3 @@ function displayArrow(ev) {
 }
 
 click.addEventListener('click', displayArrow);
-
-const click2 = document.querySelector('.click2');
-const section2 = document.querySelector('.form__section__signup');
-const arrowDown2 = document.querySelector('.moonicondown2');
-const arrowUp2 = document.querySelector('.mooniconup2');
-
-function displayArrow2(ev) {
-  ev.preventDefault();
-  section2.classList.toggle('hidden');
-  arrowUp2.classList.toggle('hidden');
-  arrowDown2.classList.toggle('hidden');
-}
-
-click2.addEventListener('click', displayArrow2);
-
-// share
-const click3 = document.querySelector('.click3');
-const section3 = document.querySelector('.form__share__button');
-const arrowDown3 = document.querySelector('.moonicondown3');
-const arrowUp3 = document.querySelector('.mooniconup3');
-
-function displayArrow3(ev) {
-  ev.preventDefault();
-  section3.classList.toggle('hidden');
-  arrowUp3.classList.toggle('hidden');
-  arrowDown3.classList.toggle('hidden');
-}
-
-click3.addEventListener('click', displayArrow3);
